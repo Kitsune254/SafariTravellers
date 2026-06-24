900a42b9824f7a4a23dc229a750cedca13612d2b
Kitsune254 <owokoselwyn@gmail.com>
2026-06-24 16:49:56 +0300

    changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.permanent_session_lifetime = timedelta(days=7)
 
 db = SQLAlchemy(app)
 
@@ -12,19 +14,55 @@ def home():
 
 @app.route('/account')
 def account():
+    # if 'user' not in session:
+    #     return redirect(url_for('login'))
     return render_template('account.html')
 
 @app.route('/feedback')
 def feedback():
     return render_template('feedback.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        # Process signup logic here
+        user = request.form['fullname']
+        email = request.form['email']
+        password = request.form['password']
+        session['user'] = user
 
-@app.route('/login')
+        saved_user = users.query.filter_by(name=user, email=email).first()
+        if saved_user:
+            return login()
+        else:
+            usr = users(name=user, email=email, password=password)
+            db.session.add(usr)
+            db.session.commit()
+            return redirect(url_for('home'))
+    else:
+        if 'user' in session:
+            return redirect(url_for('home'))
+        return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        # Process login logic here
+        email = request.form['email']
+        password = request.form['password']
+        saved_user = users.query.filter_by(email=email, password=password).first()
+        if saved_user:
+            session['user'] = saved_user.name
+            return redirect(url_for('home'))
+    else:
+        if 'user' in session:
+            return redirect(url_for('home'))
+        return render_template('login.html')
+    
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    # db.create_all()
     app.run(debug=True)
